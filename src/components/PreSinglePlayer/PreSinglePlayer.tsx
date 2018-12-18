cbaf0e623cda7592026ba59ba3e2952addbeaff2
igor-dyrov <bazbazbaz.id@gmail.com>
2018-12-18 21:15:00 +0300

    some features

--- src/components/PreSinglePlayer/PreSinglePlayer.tsx
+++ src/components/PreSinglePlayer/PreSinglePlayer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { Redirect } from 'react-router';
 
 import './PreSinglePlayer.scss';
+import { PATHS } from '../../routes';
 
 interface IProps {
     scoreTip: string;
@@ -8,11 +10,22 @@ interface IProps {
     goalTip: string;
     mainLabel: string;
     playText: string;
+    backText: string;
+    backClick?: () => void;
+    playClick?: () => void;
 }
 
-export default class PreSinglePlayer extends React.Component<IProps> {
+interface IState {
+    needRedirect: boolean;
+}
+
+export default class PreSinglePlayer extends React.Component<IProps, IState> {
     public constructor(props: IProps) {
         super(props);
+        this.state = {
+            needRedirect: false,
+        };
+        this.onBackClick = this.onBackClick.bind(this);
     }
 
     public render(): JSX.Element {
@@ -21,32 +34,44 @@ export default class PreSinglePlayer extends React.Component<IProps> {
         const {controlTip} = this.props;
         const {goalTip} = this.props;
         const {playText} = this.props;
+        const {backText} = this.props;
+        const {playClick} = this.props;
 
         return (
             <div className='singleplayer-block__before-game-block'>
-                <div className='before-game-block__logo-label'>{mainLabel}</div>
+                {!this.state.needRedirect ?
+                    <div className='before-game-block__logo-label'>{mainLabel}</div> :
+                    <Redirect to={PATHS.MENU}/>
+                }
                 <div className='before-game-block__score-tip'>
                     <img src='../../statics/imgs/score.png' className='images-block__image'/>
-                        <div className='images-block__rules-text'>
-                            {scoreTip}
-                        </div>
+                    <div className='images-block__rules-text'>
+                        {scoreTip}
+                    </div>
                 </div>
                 <div className='before-game-block__control-tip'>
                     <img src='../../statics/imgs/control.jpg' className='images-block__image'/>
-                        <div className='images-block__rules-text'>
-                            {controlTip}
-                        </div>
+                    <div className='images-block__rules-text'>
+                        {controlTip}
+                    </div>
                 </div>
                 <div className='before-game-block__goal-tip'>
                     <img src='../../statics/imgs/goal.png' className='images-block__image'/>
-                        <div className='images-block__rules-text'>
-                            {goalTip}
-                        </div>
+                    <div className='images-block__rules-text'>
+                        {goalTip}
+                    </div>
                 </div>
                 <div className='before-game-block__buttons-block'>
-                    <span className='play-button game-button'>{playText}</span>
+                    <div className='game-button' onClick={this.onBackClick}>{backText}</div>
+                    <div className='play-button game-button' onClick={playClick}>{playText}</div>
                 </div>
             </div>
         );
+    }
+
+    private onBackClick() {
+        const {backClick} = this.props;
+        this.setState({needRedirect: true});
+        backClick();
     }
 }
--- src/containers/SinglePlayer/SinglePalyer.tsx
+++ src/containers/SinglePlayer/SinglePalyer.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import PreSinglePlayer from '../../components/PreSinglePlayer/PreSinglePlayer';
 import { ILangReducer } from '../../redux/lang/lang.reducer';
+import { parseAll } from '../../service/UrlParser/UrlParser';
 
-/* tslint:disable:variable-name */
-const SinglePlayerWrapper = styled.div`
-  // some styles
-`;
-
-/* tslint:enable:variable-name */
+import './SinglePlayer.scss';
 
 interface IProps {
-
+    scoreTip: string;
+    controlTip: string;
+    goalTip: string;
+    mainLabel: string;
+    playText: string;
+    backText: string;
 }
 
 class SinglePlayer extends React.Component<IProps> {
@@ -21,9 +21,23 @@ class SinglePlayer extends React.Component<IProps> {
     }
 
     public render(): JSX.Element {
+        const {scoreTip} = this.props;
+        const {controlTip} = this.props;
+        const {goalTip} = this.props;
+        const {mainLabel} = this.props;
+        const {playText} = this.props;
+        const {backText} = this.props;
+
         return (
             <div className='singleplayer-block'>
-                <PreSinglePlayer scoreTip={''} controlTip={''} goalTip={''} mainLabel={''} playText={''}/>
+                <PreSinglePlayer
+                    scoreTip={ scoreTip }
+                    controlTip={ controlTip }
+                    goalTip={ goalTip }
+                    mainLabel={ mainLabel }
+                    playText={ playText}
+                    backText={ backText }
+                />
             </div>
         );
     }
@@ -37,10 +51,20 @@ class SinglePlayer extends React.Component<IProps> {
         const floorElements = document.getElementsByClassName('main-content__logo') as HTMLCollectionOf<HTMLElement>;
         floorElements[0].style.display = 'block';
     }
+
+    private onBackClick() {
+    }
 }
 
 const mapStateToProps = (state: { lang: ILangReducer }) => {
-    return {};
+    return {
+        scoreTip: state.lang.langObject['preSingle.scoreTip'][state.lang.lang],
+        controlTip: state.lang.langObject['preSingle.controlTip'][state.lang.lang],
+        goalTip: state.lang.langObject['preSingle.goalTip'][state.lang.lang],
+        playText: state.lang.langObject['preSingle.play'][state.lang.lang],
+        mainLabel: state.lang.langObject['preSingle.label'][state.lang.lang],
+        backText: state.lang.langObject['buttonBack'][state.lang.lang],
+    };
 };
 
 const mapDispatchToProps = dispatch => {
